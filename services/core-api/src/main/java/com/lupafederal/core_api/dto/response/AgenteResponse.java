2d4b88e374bfbb5aa2ba44d4c8b7b3bdb6546796
isaac <isaacportela0903@gmail.com>
2026-04-12 18:51:28 -0300

    feat: criação de camadas faltantes para agentes.

--- services/core-api/src/main/java/com/lupafederal/core_api/dto/response/AgenteResponse.java
+++ services/core-api/src/main/java/com/lupafederal/core_api/dto/response/AgenteResponse.java
@@ -2,7 +2,6 @@ package com.lupafederal.core_api.dto.response;
 
 public record AgenteResponse (
         Integer id,
-        Long idPortalApi,
         String nome,
         String cpfMascarado,
         Integer idOrgao
