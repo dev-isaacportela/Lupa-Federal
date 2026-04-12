d0276cd84cdf3f50611953cf38d36f12cb89bc83
isaac <isaacportela0903@gmail.com>
2026-04-12 18:52:37 -0300

    fix: correção de validação de atributos.

--- services/core-api/src/main/java/com/lupafederal/core_api/dto/request/CreateFornecedorRequest.java
+++ services/core-api/src/main/java/com/lupafederal/core_api/dto/request/CreateFornecedorRequest.java
@@ -1,10 +1,28 @@
 package com.lupafederal.core_api.dto.request;
 
 import com.lupafederal.core_api.model.TipoPessoa;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record CreateFornecedorRequest(
+        @NotNull
         Long idFornecedorApi,
+
+        @NotBlank
+        @Pattern(
+                regexp = "(^\\d{11}$)|(^\\d{14}$)",
+                message = "O campo deve ser um CPF (11 dígitos) ou um CNPJ (14 dígitos)"
+        )
         String cnpjCpf,
+
+        @NotBlank
+        @Size(min = 5, message = "O campo deve ter no mínimo 5 caracteres")
         String razaoSocial,
+
+        @NotNull(message = "O tipo da despesa é obrigatório")
         TipoPessoa tipoPessoa
-) {}
+) {
+
+}
--- services/core-api/src/main/java/com/lupafederal/core_api/dto/request/CreateOrgaoRequest.java
+++ services/core-api/src/main/java/com/lupafederal/core_api/dto/request/CreateOrgaoRequest.java
@@ -1,8 +1,22 @@
 package com.lupafederal.core_api.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record CreateOrgaoRequest (
+        @NotNull
         Long idOrgaoApi,
+
+        @NotBlank
+        @Size(min = 1, message = "O campo deve ter no mínimo 1 caracter")
         String codigoSiafi,
+
+        @NotBlank
+        @Size(min = 5, message = "O campo deve ter no mínimo 5 caracteres")
         String nome,
+
+        @NotBlank
+        @Size(min = 2, message = "O campo deve ter no mínimo 2 caracteres")
         String sigla
 ){}
